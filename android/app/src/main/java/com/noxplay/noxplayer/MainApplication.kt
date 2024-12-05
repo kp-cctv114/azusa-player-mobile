1c409dda3ed08a02b3ec137b17944d7e2f3c6420
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-12-05 11:34:28 -0800

    Update MainApplication.kt

--- android/app/src/main/java/com/noxplay/noxplayer/MainApplication.kt
+++ android/app/src/main/java/com/noxplay/noxplayer/MainApplication.kt
@@ -42,7 +42,7 @@ class MainApplication : Application(), ReactApplication {
         SoLoader.init(this,  OpenSourceMergedSoMapping)
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
             // If you opted-in for the New Architecture, we load the native entry point for this app.
-            load()
+            load(bridgelessEnabled=false)
         }
         onApplicationCreate(this)
     }
